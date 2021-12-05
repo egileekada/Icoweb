33a62fc5380d13762abef8c286716b06c99b6273
egileekada <egileoniso.ekada@gmail.com>
2021-12-05 23:30:32 +0100

    fixing styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const colors = require('tailwindcss/colors')
 
-module.exports = {
-  
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+module.exports = { 
+  purge: [],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
