10be69b57ddc52793a18bff434a7f8f7cb3c1b2a
egileekada <egileoniso.ekada@gmail.com>
2021-12-05 23:19:48 +0100

    finishing up service, selectedproject, and navbar component

---
+++ home-components/components/SelectedProjects.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SelectedProjects() {
+    return (
+        <div style={{backgroundColor: '#F4F4F4'}} className='w-full h-auto py-32 px-12 flex flex-col items-center justify-center '>
+            <p className='font-PublicSans-Bold text-2xl' >Selected  Projects</p>
+            <div className='mt-10 w-full grid grid-cols-2 gap-2' >
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
