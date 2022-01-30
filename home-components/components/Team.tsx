84b2867e00c696c9cb97515c1aa9ad3517125aff
egileekada <egileoniso.ekada@gmail.com>
2022-01-31 00:04:53 +0100

    update

--- home-components/components/Team.tsx
+++ home-components/components/Team.tsx
@@ -25,6 +25,15 @@ export default function Team() {
                     dragConstraints={{ left: -100, right: 100 }}
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}  className='w-auto' > 
+                    <Image src={'/assets/images/team2.jpg'} className='rounded-full object-cover' width='170' height='170'/>
+                    <p className='font-PublicSans-Medium text-center mt-4' >Tekevwe Emoghene</p>
+                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >Chief Operations Officer</p>
+                </motion.div>
+                <motion.div 
+                    drag="x"
+                    dragConstraints={{ left: -100, right: 100 }}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}  className='w-auto' > 
                     <Image src={'/assets/images/team1.jpg'} className='rounded-full object-cover' width='170' height='170'/>
                     <p className='font-PublicSans-Medium text-center mt-4' >George Boma Smith</p>
                     <p className='font-Poppins-Regular text-center mt-1 text-xs' >Chief Technical officer</p>
@@ -34,27 +43,18 @@ export default function Team() {
                     dragConstraints={{ left: -100, right: 100 }}
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}  className='w-auto' > 
-                    <Image src={'/assets/images/team2.jpg'} className='rounded-full object-cover' width='170' height='170'/>
-                    <p className='font-PublicSans-Medium text-center mt-4' >Tekevwe Emoghene</p>
-                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >Chief Operations Officer</p>
-                </motion.div>
-                <motion.div 
-                    drag="x"
-                    dragConstraints={{ left: -100, right: 100 }}
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}  className='w-auto' >  
-                    <Image src={'/assets/images/Bright.png'} className='rounded-full object-cover' width='170' height='170'/> 
-                    <p className='font-PublicSans-Medium text-center mt-4' >Bright Mba</p>
-                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >Product Designer</p>
+                    <Image src={'/assets/images/OBOMHENSE IDEMUDIA.jpeg'} className='rounded-full bg-gray-400 object-cover' width='170' height='170'/> 
+                    <p className='font-PublicSans-Medium text-center mt-4 mx-2' >Obmhense Idemudia</p>
+                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >CMO Business</p>
                 </motion.div>
                 <motion.div 
                     drag="x"
                     dragConstraints={{ left: -100, right: 100 }}
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}  className='w-auto' > 
-                    <Image src={'/assets/images/Avatar.png'} className='rounded-full bg-gray-400 object-cover' width='170' height='170'/> 
-                    <p className='font-PublicSans-Medium text-center mt-4' >Ekada God's Time</p>
-                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >Frontend Developer</p>
+                    <Image src={'/assets/images/Okorie Victory Ikechukwu.jpeg'} className='rounded-full bg-gray-400 object-cover object-top' width='170' height='170'/> 
+                    <p className='font-PublicSans-Medium text-center mt-4' >Okorie Victory Ikechukwu</p>
+                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >CFO</p>
                 </motion.div>
             </div>
             <div className='w-full flex lg:flex-row flex-col justify-center mt-10 items-center' > 
@@ -63,15 +63,6 @@ export default function Team() {
                     dragConstraints={{ left: -100, right: 100 }}
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}  className='w-auto mx-3' > 
-                    <Image src={'/assets/images/OBOMHENSE IDEMUDIA.jpeg'} className='rounded-full bg-gray-400 object-cover' width='170' height='170'/> 
-                    <p className='font-PublicSans-Medium text-center mt-4 mx-2' >Obmhense Idemudia</p>
-                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >CMO</p>
-                </motion.div>
-                <motion.div 
-                    drag="x"
-                    dragConstraints={{ left: -100, right: 100 }}
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.9 }}  className='w-auto mx-3 lg:mt-0 mt-6' > 
                     <Image src={'/assets/images/Aliyu Pokima.jpeg'} className='rounded-full bg-gray-400 object-cover object-top' width='170' height='170'/> 
                     <p className='font-PublicSans-Medium text-center mt-4' >Aliyu Pokima</p>
                     <p className='font-Poppins-Regular text-center mt-1 text-xs' >Product Manager</p>
@@ -80,10 +71,19 @@ export default function Team() {
                     drag="x"
                     dragConstraints={{ left: -100, right: 100 }}
                     whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}  className='w-auto mx-3 lg:mt-0 mt-6' >  
+                    <Image src={'/assets/images/Bright.png'} className='rounded-full object-cover' width='170' height='170'/> 
+                    <p className='font-PublicSans-Medium text-center mt-4' >Bright Mba</p>
+                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >Product Designer</p>
+                </motion.div>
+                <motion.div 
+                    drag="x"
+                    dragConstraints={{ left: -100, right: 100 }}
+                    whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}  className='w-auto mx-3 lg:mt-0 mt-6' > 
-                    <Image src={'/assets/images/Okorie Victory Ikechukwu.jpeg'} className='rounded-full bg-gray-400 object-cover object-top' width='170' height='170'/> 
-                    <p className='font-PublicSans-Medium text-center mt-4' >Okorie Victory Ikechukwu</p>
-                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >CFO</p>
+                    <Image src={'/assets/images/Avatar.png'} className='rounded-full bg-gray-400 object-cover' width='170' height='170'/> 
+                    <p className='font-PublicSans-Medium text-center mt-4' >Ekada God's Time</p>
+                    <p className='font-Poppins-Regular text-center mt-1 text-xs' >Frontend Developer</p>
                 </motion.div>
             </div>
         </div>
