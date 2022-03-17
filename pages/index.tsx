73dd6d9ffced21108052db272151e911e987d2c6
egileekada <egileoniso.ekada@gmail.com>
2022-03-17 21:27:22 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ export default function index() {
       <Head>
         <title>Icoweb Agency</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="p:domain_verify" content="c7200d132fcf020f2e8b8ba02a7e7489"/>
       </Head>
       <HomeComponents />
     </div>
