3020e733ed01ba74178322f2c951961c8c6cb518
egileekada <egileoniso.ekada@gmail.com>
2022-03-20 21:33:27 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../home-components/components/Navbar' 
+import Script from 'next/script'
 import HomeComponents from '../home-components/HomeComponents'
 
 export default function index() {
@@ -10,6 +11,22 @@ export default function index() {
         <title>Icoweb Agency</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="p:domain_verify" content="c7200d132fcf020f2e8b8ba02a7e7489"/>
+ 
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-10867698792"
+        // strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-10867698792');
+        `}
+      </Script>
+        {/* <!-- Global site tag (gtag.js) - Google Ads: 10867698792 --> */}
+{/* <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10867698792"></script>  */}
       </Head>
       <HomeComponents />
     </div>
