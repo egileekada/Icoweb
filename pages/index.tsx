a6bfc85c6027adf2572c68ebb08189161eb3ee07
egileekada <egileoniso.ekada@gmail.com>
2022-04-04 11:21:15 +0100

    add strategy script update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../home-components/components/Navbar' 
-import Script from 'next/script'
+// import Image from 'next/image'
+// import Navbar from '../home-components/components/Navbar' 
+// import Script from 'next/script'
 import HomeComponents from '../home-components/HomeComponents'
 
 export default function index() {
@@ -17,7 +17,7 @@ export default function index() {
         // strategy="afterInteractive"
       /> */}
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10867698792"></script> 
-      <Script id="google-analytics" strategy="afterInteractive">
+      <script>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
@@ -25,7 +25,16 @@ export default function index() {
 
           gtag('config', 'AW-10867698792');
         `}
-      </Script>
+      </script>
+      {/* <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-10867698792');
+        `}
+      </Script> */}
         {/* <!-- Global site tag (gtag.js) - Google Ads: 10867698792 --> */}
       </Head>
       <HomeComponents />
